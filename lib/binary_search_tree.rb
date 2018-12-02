024dead1d6540e9dd89fdd963a6b48b95ffa1495
nagerz <znager@gmail.com>
2018-12-02 14:59:55 -0700

    Add insert loop

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -1,19 +1,45 @@
+require './new_node'
+
 class BinarySearchTree
-  attr_reader :movies
+  attr_reader :root
 
   def initialize
-    @movies = {}
+    @root = nil
   end
 
   def insert(score, title)
-    if @movies.empty?
-    @movies[score] = title
-    less_keys_count = 0
-    @movies.each_key do |key|
-      if key < score
-        less_keys_count += 1
+    current_node = @root
+    depth = 0
+    loop do
+      if current_node.nil?
+        current_node = NewNode.new(score, title)
+        #return depth
+        break
+      else
+        depth += 1
+        if score <= current_node.score
+          if current_node.left.nil?
+            current_node.left = NewNode.new(score, title)
+            break
+          else
+            current_node = current_node.left
+            #loop
+          end
+        elsif score >= current_node.score
+          if current_node.right.nil?
+            current_node.right = NewNode.new(score, title)
+            break
+          else
+            current_node = current_node.right
+            #loop
+          end
+        else
+          return "Duplicate score attempted"
+          #break
+        end
       end
-    end
-
+      return depth
+      end
   end
+
 end
