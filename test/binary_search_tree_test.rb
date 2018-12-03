f76f894a949458bc04562719e64db07e484a296c
nagerz <znager@gmail.com>
2018-12-03 00:11:17 -0700

    Add include method and test

--- test/binary_search_tree_test.rb
+++ test/binary_search_tree_test.rb
@@ -56,14 +56,15 @@ class BinarySearchTreeTest < Minitest::Test
     assert_equal nil, tree.root.left.right.left
   end
 
-  # def test_tree_include
-  #   tree = BinarySearchTree.new
-  #   tree.insert(61, "Bill & Ted's Excellent Adventure")
-  #   tree.insert(16, "Johnny English")
-  #   tree.insert(92, "Sharknado 3")
-  #   tree.insert(50, "Hannibal Buress: Animal Furnace")
-  #
-  #   assert_equal true, tree.include?(16)
-  #   assert_equal false, tree.include?(72)
-  # end
+  def test_tree_include
+    tree = BinarySearchTree.new
+    tree.insert(61, "Bill & Ted's Excellent Adventure")
+    tree.insert(16, "Johnny English")
+    tree.insert(92, "Sharknado 3")
+    tree.insert(50, "Hannibal Buress: Animal Furnace")
+
+    assert_equal true, tree.include?(16)
+    assert_equal true, tree.include?(50)
+    assert_equal false, tree.include?(72)
+  end
 end
